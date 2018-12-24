aab25ea7e2411898f18a1a19a1556bb7102385ac
Harvey Zack <laozhaochaguan@gmail.com>
2018-12-24 17:49:21 +0800

    添加控制台输出

--- docs/js/common.js
+++ docs/js/common.js
@@ -5,6 +5,19 @@
     var $fileInput = document.querySelector('.fileInput');
     var $jsonTree = document.querySelector('.jsonTree');
 
+    consola.creat({
+        target: '.console',
+        size: '100%',
+        zIndex: 99,
+    });
+
+    console.info('Welcome, if you like it, consider star it, thank you.');
+    console.info('https://github.com/zhw2590582/flv-reader');
+
+    window.addEventListener('error', function(err) {
+        console.error(err.message);
+    });    
+
     function creatPlayer(url) {
         Flv.instances.forEach(function(flv) {
             flv.destroy();
